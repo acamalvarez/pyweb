2f66bce15e341449b4017844c17e79eb03efeb6f
acamalvarez <acam@outlook.com>
2020-08-02 12:21:22 -0500

    first commit

---
+++ index.py
@@ -0,0 +1,35 @@
+import tornado.web
+import tornado.ioloop
+
+class basicRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world! This is my first tornado tutorial.")
+
+class resourceRequestHandler(tornado.web.RequestHandler):
+    def get(self, id):
+        self.write("Querying tweet with id " + id)
+
+class queryStringRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        n = int(self.get_argument("n"))
+        r = "odd" if n % 2 else "even"
+
+        self.write("the number " + str(n) + " is " + r)
+
+class staticRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", basicRequestHandler),
+        (r"/blog", staticRequestHandler),
+        (r"/isEven", queryStringRequestHandler),
+        (r"/tweet/([0-9]+)", resourceRequestHandler)
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8881)
+    print("I'm listeing on port 8881")
+    tornado.ioloop.IOLoop.current().start()
